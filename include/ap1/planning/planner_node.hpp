198e1075311f88801bec13d7c66743066e272326
Aly Ashour <79553148+alyashour@users.noreply.github.com>
2025-11-04 20:19:47 -0500

    Colcon script and automated tests. (#3)
    * yaml script adjusted for colcon
    
    * Manual dispatch added
    
    * Branch added to push:branches:
    
    * Yaml now installs ros 2 at runtime
    
    * Release tag changed and jazzy added
    
    * Now running on github hosted Ubuntu runner for sudo permissions
    
    * sudo rosdep update changed to initialize rosdep installation
    
    * Sourcing of ros2 enivronment added
    
    * Set local hosteed-runner and remove steps to setup ros 2
    
    * Changed tag to selgf-hosted
    
    * Resolved white space and package selection in yaml file
    
    * uncrustify reformat
    
    ---------
    
    Co-authored-by: Logan Ouellette <loganouellettetran@gmail.com>

--- include/ap1/planning/planner_node.hpp
+++ include/ap1/planning/planner_node.hpp
@@ -15,66 +15,78 @@
 #include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/point32.hpp"
 
-namespace ap1::planning {
-    class PlannerNode : public rclcpp::Node {
-    public:
-        PlannerNode() : Node("planner_node") {
+namespace ap1::planning
+{
+class PlannerNode : public rclcpp::Node {
+public:
+  PlannerNode()
+  : Node("planner_node")
+  {
             // # Subscribe to all inputs
             // - HD MAP
-            hd_map_sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+    hd_map_sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
                     "hd_map", 10, std::bind(&PlannerNode::on_hd_map, this, std::placeholders::_1));
             // - TURN ANGLE
-            turn_angle_sub_ = this->create_subscription<std_msgs::msg::Float32>(
-                    "turn_angle", 10, std::bind(&PlannerNode::on_turn_angle, this, std::placeholders::_1));
+    turn_angle_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+                    "turn_angle", 10,
+        std::bind(&PlannerNode::on_turn_angle, this, std::placeholders::_1));
             // - VEHICLE SPEED
-            vehicle_speed_sub_ = this->create_subscription<std_msgs::msg::Float32>(
-                    "vehicle_speed", 10, std::bind(&PlannerNode::on_vehicle_speed, this, std::placeholders::_1));
+    vehicle_speed_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+                    "vehicle_speed", 10,
+        std::bind(&PlannerNode::on_vehicle_speed, this, std::placeholders::_1));
             // - TARGET LOCATION
-            target_location_sub_ = this->create_subscription<geometry_msgs::msg::Point>(
-                    "target_location", 10, std::bind(&PlannerNode::on_target_location, this, std::placeholders::_1));
+    target_location_sub_ = this->create_subscription<geometry_msgs::msg::Point>(
+                    "target_location", 10,
+        std::bind(&PlannerNode::on_target_location, this, std::placeholders::_1));
 
             // # Publishers
             // - SPEED PROFILE
-            speed_profile_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("speed_profile", 10);
-            // - TARGET PATH 
-            target_path_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("target_path", 10);
+    speed_profile_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("speed_profile",
+        10);
+            // - TARGET PATH
+    target_path_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("target_path", 10);
 
-            RCLCPP_INFO(this->get_logger(), "Path Planner Node initialized");
-        }
-    private:
+    RCLCPP_INFO(this->get_logger(), "Path Planner Node initialized");
+  }
+
+private:
         // # Callbacks
-        void on_hd_map(const std_msgs::msg::Float32MultiArray::SharedPtr) {
+  void on_hd_map(const std_msgs::msg::Float32MultiArray::SharedPtr)
+  {
             // todo: implement
-            RCLCPP_INFO(this->get_logger(), "Received HD Map");
-        }
+    RCLCPP_INFO(this->get_logger(), "Received HD Map");
+  }
 
-        void on_turn_angle(const std_msgs::msg::Float32::SharedPtr) {
+  void on_turn_angle(const std_msgs::msg::Float32::SharedPtr)
+  {
             // todo: implement
-            RCLCPP_INFO(this->get_logger(), "Received current car turn angle");
-        }
+    RCLCPP_INFO(this->get_logger(), "Received current car turn angle");
+  }
 
-        void on_vehicle_speed(const std_msgs::msg::Float32::SharedPtr) {
+  void on_vehicle_speed(const std_msgs::msg::Float32::SharedPtr)
+  {
             // todo: implement
-            RCLCPP_INFO(this->get_logger(), "Received current vehicle speed");
-        }
+    RCLCPP_INFO(this->get_logger(), "Received current vehicle speed");
+  }
 
-        void on_target_location(const geometry_msgs::msg::Point::SharedPtr msg) {
+  void on_target_location(const geometry_msgs::msg::Point::SharedPtr msg)
+  {
             // todo: implement
-            RCLCPP_INFO(this->get_logger(), "Target location received: (%.2f, %.2f, %.2f)",
+    RCLCPP_INFO(this->get_logger(), "Target location received: (%.2f, %.2f, %.2f)",
                     msg->x, msg->y, msg->z);
-        }
+  }
 
         // # Subscriptions
         // Note: use SharedPtrs for all messages since they're dynamically allocated in ROS
-        rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr hd_map_sub_;
-        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr turn_angle_sub_;
-        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vehicle_speed_sub_;
-        rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr target_location_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr hd_map_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr turn_angle_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vehicle_speed_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr target_location_sub_;
 
         // # Publishers
-        rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr speed_profile_pub_;
-        rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr target_path_pub_;
-    };
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr speed_profile_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr target_path_pub_;
+};
 }
 
 #endif // AP1_PLANNING_NODE_HPP
